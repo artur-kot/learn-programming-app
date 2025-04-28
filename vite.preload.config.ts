76231194a6a0989bd911cdac8be5b6a63f8ee8f5
Artur Kot <arturkot95@gmail.com>
2025-04-28 19:27:10 +0200

    add prettier, viteTsConfigPaths

--- vite.preload.config.ts
+++ vite.preload.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config
 export default defineConfig({
+  plugins: [
+    viteTsConfigPaths(),
+  ],
   build: {
     lib: {
       entry: 'src/main.ts',
       name: 'main',
-      formats: ['es']
-    }
+      formats: ['es'],
+    },
   },
 });
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config
 export default defineConfig({
+  plugins: [
+    viteTsConfigPaths(),
+  ],
   build: {
     lib: {
       entry: 'src/main.ts',
       name: 'main',
-      formats: ['es']
-    }
+      formats: ['es'],
+    },
+    sourcemap: true,
   },
 });
