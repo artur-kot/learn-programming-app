bdb1ce36430b7d9e4d9b31d9c7e0b5fdc760cfb9
Artur Kot <arturkot95@gmail.com>
2025-08-14 19:56:02 +0200

    initialize vue and make it work

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config
 export default defineConfig({
   plugins: [
     viteTsConfigPaths(),
+    vue()
   ],
   build: {
     lib: {
-      entry: 'src/renderer.tsx',
+      entry: 'src/renderer.ts',
       name: 'main',
       formats: ['es'],
     },
