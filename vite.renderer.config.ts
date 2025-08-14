853dfc054d7444a674bad7389b135ed0d14c281d
Artur Kot <arturkot95@gmail.com>
2025-08-14 20:09:50 +0200

    install and init tailwindcss

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
+
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vitejs.dev/config
 export default defineConfig({
   plugins: [
     viteTsConfigPaths(),
-    vue()
+    tailwindcss(),
+    vue(),
   ],
   build: {
     lib: {
