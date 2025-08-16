cf38195e0bd50310af38789a74a2ee17933f4b09
Artur Kot <arturkot95@gmail.com>
2025-08-16 16:12:36 +0200

    add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <h1 class="font-bold">💖 Hello World!</h1>
-  <p>Welcome to your Electron application.</p>
+  <h1>Hello App!</h1>
+  <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
+  <nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/about">Go to About</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
-
-<script setup>
-console.log('👋 This message is being logged by "App.vue", included via Vite');
-</script>
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './styles.css'
+import router from './routes.js'
 
-createApp(App).mount('#app');
+createApp(App).use(router).mount('#app');
