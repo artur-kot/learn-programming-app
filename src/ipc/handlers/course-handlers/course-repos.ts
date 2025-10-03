07caa4a0c721d64422fe6a7a0eae55c4d223b2d4
Artur Kot <artur.kot@outlook.com>
2025-10-03 22:03:45 +0200

    init tauri project

--- src/ipc/handlers/course-handlers/course-repos.ts
+++
@@ -1,3 +0,0 @@
-export const courseSlugToRepoUrl = {
-  javascript: 'https://github.com/ArturKot95/learn-programming-javascript.git',
-} as const;
--- src/ipc/handlers/index.ts
+++
@@ -1,7 +0,0 @@
-import { themeHandlers } from './theme.handlers.js';
-import { gitCourseHandlers } from './course-handlers/git.course.handlers.js';
-
-export const ipcHandlers = {
-  ...themeHandlers,
-  ...gitCourseHandlers,
-};
